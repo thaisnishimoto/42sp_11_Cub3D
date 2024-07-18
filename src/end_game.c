9afae7d38ed14c26e89bd9e923c6100138477e0c
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-07-18 19:55:23 -0300

    Added mlx ending to handle error

--- src/end_game.c
+++ src/end_game.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 13:46:22 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/07/18 14:55:44 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:12:34 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@ void	handle_error(char *msg, t_data *game, int stage)
 	write(STDERR_FILENO, msg, ft_strlen(msg));
 	write(STDERR_FILENO, "\n", 1);
 	if (stage > 0)
+	{
 		ft_free_matrix(game->map.content);
+		if (stage > 1)
+		{
+			mlx_close_window(game->mlx);
+			mlx_terminate(game->mlx);
+			free(game);
+		}
+	}
 	exit(EXIT_FAILURE);
 }
 
@@ -28,15 +36,11 @@ void	end_game(void *param)
 
 	game = param;
 	mlx_close_window(game->mlx);
-	mlx_delete_image(game->mlx, game->background_img);
-	mlx_delete_image(game->mlx, game->minimap_img);
-	mlx_delete_image(game->mlx, game->player_img);
+//	mlx_delete_image(game->mlx, game->background_img);
+//	mlx_delete_image(game->mlx, game->minimap_img);
+//	mlx_delete_texture;
 	mlx_terminate(game->mlx);
 	ft_free_matrix(game->map.content);
 	free(game);
-//	mlx_delete_texture;
-//	for handle_error call
-//	if (game->config_stage < 4)
-//		return (1);
 	exit (EXIT_SUCCESS);
 }
