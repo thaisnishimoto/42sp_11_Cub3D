1de6c12f785e351a6859a673ddf541b3c69cf639
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-07-18 19:57:08 -0300

    Rename variables to norm

--- src/draw_background.c
+++ src/draw_background.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:47:21 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/07/18 14:38:09 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:29:17 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	draw_background(t_data *game)
 	int	x;
 	int	y;
 
-	game->background_img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
-	if (!game->background_img)
+	game->back_img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	if (!game->back_img)
         	handle_error("Background image creation failed", game, 1);
 	y = 0;
 	while (y < HEIGHT)
@@ -27,14 +27,14 @@ void	draw_background(t_data *game)
 		while (x < WIDTH)
 		{
 			if (y < HEIGHT / 2)
-				mlx_put_pixel(game->background_img, x, y, game->map.ceiling_color);
+				mlx_put_pixel(game->back_img, x, y, game->map.ceiling_color);
 			else
-				mlx_put_pixel(game->background_img, x, y, game->map.floor_color);
+				mlx_put_pixel(game->back_img, x, y, game->map.floor_color);
 			x++;
 		}
 		y++;
 	}
-	if (mlx_image_to_window(game->mlx, game->background_img, 0, 0) < 0)
+	if (mlx_image_to_window(game->mlx, game->back_img, 0, 0) < 0)
         	handle_error("Background image render failed", game, 1);
-	game->background_img->instances->z = 1;
+	game->back_img->instances->z = 1;
 }
--- src/weapon.c
+++ src/weapon.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 16:12:43 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/07/18 12:48:19 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:50:35 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,30 @@
 
 void	load_weapon_textures(t_data *game)
 {
+	t_coord	pos;
 	int	i;
 
-	game->map.weapon_texture[0] = mlx_load_png("./textures/weapon/wand_0.png");
-	game->map.weapon_texture[1] = mlx_load_png("./textures/weapon/wand_1.png");
-	game->map.weapon_texture[2] = mlx_load_png("./textures/weapon/wand_2.png");
-	game->map.weapon_texture[3] = mlx_load_png("./textures/weapon/wand_3.png");
-	game->map.weapon_texture[4] = mlx_load_png("./textures/weapon/wand_4.png");
+	game->wand_texture[0] = mlx_load_png("./textures/weapon/wand_0.png");
+	game->wand_texture[1] = mlx_load_png("./textures/weapon/wand_1.png");
+	game->wand_texture[2] = mlx_load_png("./textures/weapon/wand_2.png");
+	game->wand_texture[3] = mlx_load_png("./textures/weapon/wand_3.png");
+	game->wand_texture[4] = mlx_load_png("./textures/weapon/wand_4.png");
 	i = 0;
 	while (i < WEAPON_FRAMES)
 	{
-		game->weapon_img[i] = mlx_texture_to_image(game->mlx, game->map.weapon_texture[i]);
-		//handle_error("Player image render failed.", 0);
-		game->weapon_img[i]->enabled = false;
-		mlx_image_to_window(game->mlx, game->weapon_img[i], WIDTH / 2, HEIGHT - game->weapon_img[i]->height);
-		game->weapon_img[i]->instances->z = 5;
+		game->wand_img[i] = mlx_texture_to_image(game->mlx, game->wand_texture[i]);
+		if (!game->wand_img[i])
+			handle_error("Wand image creation failed", game, 2);
+		game->wand_img[i]->enabled = false;
+		pos.x = WIDTH / 2;
+		pos.y = HEIGHT - game->wand_img[i]->height;
+		if (mlx_image_to_window(game->mlx, game->wand_img[i], pos.x, pos.y) < 0)
+			handle_error("Weapon image render failed", game, 2);
+		game->wand_img[i]->instances->z = 4;
 		i++;
 	}
-	while (i >= 0)
-	{
-		//handle_error
-		if (game->weapon_img[i] == NULL)
-			printf("Weapon loading failed\n");
-		i--;
-	}
 	game->shoot = false;
-	game->weapon_img[0]->enabled = true;
+	game->wand_img[0]->enabled = true;
 }
 
 void	mouse_click(void *param)
@@ -58,28 +56,28 @@ void	shoot_weapon(t_data *game)
 	if (frame >= 0 && frame <= REPEAT_FRAME)
 	{
 		game->shoot = true;
-		game->weapon_img[0]->enabled = false;
-		game->weapon_img[1]->enabled = true;
+		game->wand_img[0]->enabled = false;
+		game->wand_img[1]->enabled = true;
 	}
 	if (frame > REPEAT_FRAME && frame <= REPEAT_FRAME * 2)
 	{
-		game->weapon_img[1]->enabled = false;
-		game->weapon_img[2]->enabled = true;
+		game->wand_img[1]->enabled = false;
+		game->wand_img[2]->enabled = true;
 	}
 	if (frame > REPEAT_FRAME * 2 && frame <= REPEAT_FRAME * 3)
 	{
-		game->weapon_img[2]->enabled = false;
-		game->weapon_img[3]->enabled = true;
+		game->wand_img[2]->enabled = false;
+		game->wand_img[3]->enabled = true;
 	}
 	if (frame > REPEAT_FRAME * 3 && frame <= REPEAT_FRAME * 4)
 	{
-		game->weapon_img[3]->enabled = false;
-		game->weapon_img[4]->enabled = true;
+		game->wand_img[3]->enabled = false;
+		game->wand_img[4]->enabled = true;
 	}
 	if (frame > REPEAT_FRAME * 4)
 	{
-		game->weapon_img[4]->enabled = false;
-		game->weapon_img[0]->enabled = true;
+		game->wand_img[4]->enabled = false;
+		game->wand_img[0]->enabled = true;
 		frame = 0;
 		game->shoot = false;
 	}
