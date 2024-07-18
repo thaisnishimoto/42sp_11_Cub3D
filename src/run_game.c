ee613569af43e9bb4eca9d82bf648b547725d9a4
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-07-18 19:54:35 -0300

    Rename init function

--- src/run_game.c
+++ src/run_game.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_game.c                                        :+:      :+:    :+:   */
+/*   run_game.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:47:21 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/07/18 15:23:11 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:34:19 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,18 @@ void	cursor_movement(double mouse_x, double mouse_y, void *param)
 	old_x = mouse_x;
 }
 
-void	init_game(t_data *game)
+void	run_game(t_data *game)
 {
 	game->mlx = mlx_init(WIDTH, HEIGHT, "Cub3D", false);
 	if (!game->mlx)
         	handle_error("Mlx init failed", game, 1);
-	draw_background(game);
-	draw_minimap(game);
 	load_weapon_textures(game);
+	draw_background(game);
 	mlx_close_hook(game->mlx, end_game, game);
 	mlx_loop_hook(game->mlx, key_press, game);
 	mlx_cursor_hook(game->mlx, cursor_movement, game);
+	mlx_loop_hook(game->mlx, draw_minimap, game);
 	mlx_loop_hook(game->mlx, mouse_click, game);
-	mlx_loop_hook(game->mlx, draw_miniplayer, game);
 	mlx_loop_hook(game->mlx, raycast, game);
 	mlx_loop(game->mlx);
 }
